1da215721832dd630f7301afaa7242bd7e0b1b32
Anelizi <anelizicarvalho@gmail.com>
2023-08-28 21:56:50 -0300

    feat: get /health

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -15,10 +15,10 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/health (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/health')
       .expect(200)
-      .expect('Hello World!');
+      .expect("I'm okay!");
   });
 });
